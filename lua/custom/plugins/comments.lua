8aa90711783acdf3baec83d9efa116d03ac911f2
Joakim Lorentz <joakim@murmel.se>
2024-11-02 14:21:03 +0100

    updates and configs

---
+++ lua/custom/plugins/comments.lua
@@ -0,0 +1,71 @@
+return {
+  {
+    'numToStr/Comment.nvim',
+    event = { 'BufReadPost', 'BufNewFile' },
+    dependencies = {
+      {
+        'JoosepAlviste/nvim-ts-context-commentstring',
+        dependencies = { 'nvim-treesitter/nvim-treesitter' },
+        lazy = true,
+        opts = {
+          enable_autocmd = false,
+        },
+      },
+    },
+    opts = {
+      ---Add a space b/w comment and the line
+      padding = true,
+      ---Whether the cursor should stay at its position
+      sticky = true,
+
+      ---Line which should be ignored while comment/uncomment
+      ---Example: Use '^$' to ignore empty lines
+      ignore = nil,
+      toggler = {
+        -- line-comment keymap
+        line = 'gcc',
+
+        --  Make sense to be related to your opleader.block
+        block = 'gbc',
+      },
+      -- LHS of operator-pending mapping in NORMAL + VISUAL mode
+      opleader = {
+        -- line-comment keymap
+        line = 'gc',
+        -- block-comment keymap
+        block = 'gb',
+      },
+      -- extra mapping
+      -- Includes `gco`, `gcO`, `gcA`
+      extra = {
+        ---Add comment on the line above
+        above = 'gcO',
+        ---Add comment on the line below
+        below = 'gco',
+        ---Add comment at the end of line
+        eol = 'gcA',
+      },
+
+      -- Create basic (operator-pending) and extended mappings for NORMAL + VISUAL mode
+      mappings = {
+
+        -- operator-pending mapping
+        -- Includes:
+        --  `gcc`               -> line-comment  the current line
+        --  `gcb`               -> block-comment the current line
+        --  `gc[count]{motion}` -> line-comment  the region contained in {motion}
+        --  `gb[count]{motion}` -> block-comment the region contained in {motion}
+        basic = true,
+
+        ---Extra mapping; `gco`, `gcO`, `gcA`
+        extra = true,
+      },
+    },
+    config = function(_, opts)
+      -- to skip backwards compatibility routines and speed up loading
+      opts.pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()
+      vim.g.skip_ts_context_commentstring_module = true
+      require('Comment').setup(opts)
+    end,
+  },
+}
---
+++ lua/custom/plugins/mason.lua
@@ -0,0 +1,37 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+
+return {
+  'williamboman/mason.nvim',
+  dependencies = {
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+  },
+  config = function()
+    local mason = require 'mason'
+    local mason_tool_installer = require 'mason-tool-installer'
+
+    -- enable mason and configure icons
+    mason.setup {
+      ui = {
+        icons = {
+          package_installed = '✓',
+          package_pending = '➜',
+          package_uninstalled = '✗',
+        },
+      },
+    }
+
+    mason_tool_installer.setup {
+      ensure_installed = {
+        'prettier', -- prettier formatter
+        'stylua', -- lua formatter
+        'isort', -- python formatter
+        'black', -- python formatter
+        'pylint', -- python linter
+        'eslint_d', -- js linter
+        'golangci-lint', -- go linter
+      },
+    }
+  end,
+}
